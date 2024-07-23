31bd8778a45e39b95ac870ed50d2bfe1152f11fc
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2024-07-23 14:44:40 -0500

    good animation progress

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
-console.log('Hello, world! mainjs');
+let heroTextTl;
+
+function init() {
+  heroTextObserver();
+  heroTextAnimation();
+}
+init();
+
+function heroTextAnimation() {
+  heroTextTl = gsap.timeline({paused: true});
+
+  heroTextTl.fromTo('.hero-text',
+    {
+      scale: 0.95,
+    },
+    {
+      scale: 1,
+    },
+  );
+  heroTextTl.from('.hero-text__text',
+    {
+      duration: 1,
+      x: -50,
+      opacity: 0,
+      stagger: {
+        each: 0.2,
+      },
+    },
+    ">-0.5",
+  );
+}
+
+function heroTextObserver() {
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        if (entry.intersectionRatio === 1) {
+          heroTextTl.play();
+        }
+      }
+    });
+  }, { threshold: [0, 1] });
+  
+  let element = document.querySelector('.hero-text');
+  observer.observe(element);
+}
