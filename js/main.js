fff39e0f8f4cddf1cc49dc04385e52bf088f7ca2
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2024-07-24 08:20:10 -0500

    animation progress

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
 let heroTextTl;
+let missionTextTl;
 
 function init() {
-  heroTextObserver();
-  heroTextAnimation();
+  heroText();
+  missionText();
 }
 init();
+
+function heroText() {
+  heroTextObserver();
+  heroTextAnimation();  
+}
 
 function heroTextAnimation() {
   heroTextTl = gsap.timeline({paused: true});
 
+  // Circle
+  heroTextTl.fromTo('.circle-left img',
+    {
+      x: -50,
+      opacity: 0,
+      scale: 0.9
+    },
+    {
+      x: 0,
+      scale: 1,
+      opacity: 1,
+      duration: 0.5,
+    },
+  );
+
+  // Text wrapper
   heroTextTl.fromTo('.hero-text',
     {
       scale: 0.95,
@@ -16,14 +38,17 @@ function heroTextAnimation() {
     {
       scale: 1,
     },
+    ">-0.5",
   );
+
+  // Text words
   heroTextTl.from('.hero-text__text',
     {
-      duration: 1,
-      x: -50,
+      duration: 1.5,
+      x: 50,
       opacity: 0,
       stagger: {
-        each: 0.2,
+        each: 0.1,
       },
     },
     ">-0.5",
@@ -43,4 +68,41 @@ function heroTextObserver() {
   
   let element = document.querySelector('.hero-text');
   observer.observe(element);
+}
+
+function missionText() {
+  missionTextObserver();
+  missionTextAnimation();
+}
+
+function missionTextObserver() {
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        if (entry.intersectionRatio === 1) {
+          missionTextTl.play();
+        }
+      }
+    });
+  }, { threshold: [0, 1] });
+  
+  let element = document.querySelector('.mission-text');
+  observer.observe(element);
+}
+
+function missionTextAnimation() {
+  missionTextTl = gsap.timeline({paused: true});
+  const missionText = document.querySelectorAll('.mission-text__text');
+
+  // Text wrapper
+  missionTextTl.from(missionText,
+    {
+      duration: 1,
+      x: 50,
+      opacity: 0,
+      stagger: {
+        each: 0.1,
+      },
+    },
+  );
 }
